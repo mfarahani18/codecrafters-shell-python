e3d3076ef36c60a23682b1d07b10de5c535b4c1a
Mahdi <m.farahani1412@gmail.com>
2026-08-31 18:50:55 +0330

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,34 +2,41 @@ from app.shell import Shell
 import sys
 
 
-
 def main():
 
-    
     my_shell = Shell()
-    
+
     while True:
+
         sys.stdout.write("$ ")
+
         user_input = input()
-        
+
         parts = my_shell.parse_input(user_input)
+
         command = parts[0]
         args = parts[1:]
-        
-        if command in my_shell.commands:
-            if ">" in args:
-                        idx = args.index(">")
-                        file_name = args[idx + 1]
-                        real_args = args[:idx]
-                        my_shell.redirect(command, real_args, file_name)
-            else:
-                result = my_shell.commands[command](*args)
-                
-                if result is not None:
-                    print(result)
+
+        if ">" in args:
+
+            idx = args.index(">")
+
+            file_name = args[idx + 1]
+            real_args = args[:idx]
+
+            my_shell.redirect(command, real_args, file_name)
+
+        elif command in my_shell.commands:
+
+            result = my_shell.commands[command](*args)
+
+            if result is not None:
+                print(result)
+
         else:
+
             my_shell.run_not_found(command, *args)
-            
+
 
 if __name__ == "__main__":
     main()
