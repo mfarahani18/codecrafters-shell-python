a693b3b71286f8ade102902428fba043f1c3c505
Mahdi <m.farahani1412@gmail.com>
2026-09-12 13:08:27 +0330

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -28,8 +28,9 @@ def main():
 
         if args[-1] == "&":
             process = subprocess.Popen([command, *args[:-1]])
-            print(process.pid)
+            print(f"[1] {process.pid}")
             continue
+        
         redirected = False
 
         redirect_symbols = [">", "1>", "2>", ">>", "1>>", "2>>"]
