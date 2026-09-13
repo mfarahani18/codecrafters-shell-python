98b54c31f3c55c9c73746d1414de08ceb83db18d
Mahdi <m.farahani1412@gmail.com>
2026-09-13 11:11:40 +0330

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -56,6 +56,7 @@ def main():
                 )
                 
                 process1.communicate()
+                result = my_shell.commands[right_command](*right_args)
                         
             else:
                 process1 = subprocess.Popen(
